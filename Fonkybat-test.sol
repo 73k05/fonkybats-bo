d4883d6702f63ea5960a2379fd1e0a738fc5088a
baboon <92304020+babsfub@users.noreply.github.com>
2021-10-18 19:40:30 +0200

    Update Fonkybat-test.sol
    security update

--- Fonkybat-test.sol
+++ Fonkybat-test.sol
@@ -1,19 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "./@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "./@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "./@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "./@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "./Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Fonkybat is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    constructor() ERC721("fonkybat", "FKB") {}
+    using Counters for Counters.Counter;
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("fonkybat", "HRE") {}
+
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+    
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://";
+    }
+    function setMintPrice(uint _mintPrice) external onlyOwner {
+        mintPrice = _mintPrice;
     }
 
-    // The following functions are overrides required by Solidity.
+    function setSaleState(SaleState _saleState) external onlyOwner {
+        saleState = _saleState;
+    }
+
+    function setMaxNFTPerMint(uint _maxNFTPerMint) external onlyOwner {
+        maxNFTPerMint = _maxNFTPerMint;
+    }
+
+    function setMaxSupply(uint _maxSupply) external onlyOwner {
+        maxSupply = _maxSupply;
+    }
+     function configure(uint _mintPrice, uint _maxNFTPerMint, SaleState _saleState, uint _maxSupply) external onlyOwner {
+        mintPrice = _mintPrice;
+        maxNFTPerMint = _maxNFTPerMint;
+        saleState = _saleState;
+        maxSupply = _maxSupply;
+    }
+    function safeMint(address to) public onlyOwner {
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
@@ -43,4 +77,13 @@ contract Fonkybat is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     {
         return super.supportsInterface(interfaceId);
     }
+    function withdraw() external onlyOwner {
+        uint balance = address(this).balance;
+        uint share1 = balance * 1 / 100;
+        uint share2 = balance * 15 / 100;
+        payable(0XXXXXXX).transfer(share1);
+        payable(0xAXXXXXXXXX).transfer(share1);
+        payable(Owner).transfert(share2);
+        payable(0xXXXXXXXX).transfer(balance - (share1 *2) - (share2));
+            }
 }
